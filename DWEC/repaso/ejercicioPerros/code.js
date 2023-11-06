27422a3ef0b1ed30a1eefdaf0b8bcecbc7cc072d
JuanAP99 <alconlebrija@gmail.com>
2023-11-06 21:19:56 +0100

    Planteo ejercicio Perros

---
+++ DWEC/repaso/ejercicioPerros/code.js
@@ -0,0 +1,34 @@
+"use stritc";
+
+/* Tengo que:
+
+1º mirar como se estructura un archivo con objetos y tal
+
+2º Crear el objeto perro con sus atributos, y metodo 
+mostrarDatos().
+
+3º Una vez creado el constructor de la clase darle funcionalidad
+a cada uno de los botones
+
+    Añadir: 
+
+    Ordena por insercción:
+
+    Ordena por edad:
+
+    Ordenar por raza:
+
+*/ 
+
+
+///////////////////
+//// Funciones ////
+///////////////////
+
+function anadir(){
+
+}
+
+///////////////////
+//// Main ////////
+///////////////////
