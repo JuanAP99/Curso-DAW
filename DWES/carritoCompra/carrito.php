5f7e0f75f0969e31a21e38253eefd9ee56ef985c
JuanAP99 <alconlebrija@gmail.com>
2023-11-11 23:07:22 +0100

    avance carrito

--- DWES/carritoCompra/carrito.php
+++ DWES/carritoCompra/carrito.php
@@ -2,13 +2,39 @@
 
 session_start();
 
+require_once("vars.php");
+
 if($_SESSION['nameUser'] != 'juanap99'){
     header("location:login.php");
 }else {
     $_SESSION['listaProductos'] = $_POST;
-    echo '<pre>';
-    print_r($_SESSION['listaProductos']);
-    echo '</pre>';
+    
+    foreach($_SESSION['listaProductos']['producto'] as $clave => $valor){
+
+        if (!empty($valor)){
+            $productos[$clave+1]['cantidad'] = $valor;
+        }
+
+
+    }
+    
+    $_SESSION['carrito']= [];
+
+    foreach($productos as $clave => $valor){
+
+        if($productos[$clave]['cantidad'] != 0){
+
+            $_SESSION['carrito']= $productos[$clave];
+
+        }
+    }
+
+    /* En carrito ya tengo todo lo que he seleccionado arteriormente */
+
+
+  echo '<pre>';
+  print_r($carrito);
+  echo '</pre>';
 }
 
 
