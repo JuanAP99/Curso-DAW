2457c7460c73632d65cd14588c9c6f6b8312a7f0
JuanAP99 <alconlebrija@gmail.com>
2023-11-14 20:04:40 +0100

    empezado el ejercicio de bicicletas

---
+++ DWEC/Ejercicio examen bicicletas/Plantilla examen/js/codigo.js
@@ -0,0 +1,21 @@
+﻿"use stritc";
+/* .d-none clase para ocultar cosas en boostrap */
+
+//////////////////
+//// Functions
+//////////////////
+
+const ocultarFormularios = () =>{
+    document.querySelectorAll("form").forEach(
+        formulario => formulario.classList.add("oculto")
+    );
+}
+
+
+
+////////////////
+//// Main 
+////////////////
+
+
+ocultarFormularios();
