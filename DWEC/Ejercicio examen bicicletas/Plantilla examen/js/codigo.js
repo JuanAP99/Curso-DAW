5f4be3a39c976b329f587ab210b93603f6d18600
JuanAP99 <alconlebrija@gmail.com>
2023-11-30 12:15:27 +0100

    estudiando

--- DWEC/Ejercicio examen bicicletas/Plantilla examen/js/codigo.js
+++ DWEC/Ejercicio examen bicicletas/Plantilla examen/js/codigo.js
@@ -1,1 +1,80 @@
-﻿
+﻿"use strict";
+
+function ocultarFormularios(){
+
+    const formularios = document.querySelectorAll(".form-horizontal");
+    formularios.forEach(function(formulario){
+        formulario.classList.add("oculto");
+    });
+}
+function altaBicicleta(){
+
+    let b;
+    const localizador = document.querySelector('[name="txtLocalizador"]').value;
+    const año = document.querySelector('[name="lstAnio"]').value;
+    const tipo = document.querySelector('input[name="rbtTipoBicicleta"]:checked').value;
+
+    if(tipo === "carretera") {
+        const numPlatos = document.querySelector('[name="txtPlatos"]').value;
+        b = new Carretera(localizador,año,false,numPlatos);
+    }else{
+        const numSuspensiones = document.querySelector('[name="txtSuspensiones"]').value;
+        b = new Carretera(localizador,año,false,numPlatos);
+    }
+
+    if(tienda.altaBicicleta(b)){
+        alert("Alta bicicleta Ok");
+        refrescarTotales();
+        ocultarFormularios();
+
+    }else{
+        alert("Bicicleta registrada previamente");
+        ocultarFormularios();
+    }
+
+}
+function ventaBici(){
+
+    const localizador = document.querySelector('[name="txtLocalizadorVenta"]').value;
+    alert(tienda.ventaBicis(localizador));
+    refrescarTotales();
+
+}
+function refrescarTotales(){
+
+    document.querySelector("#totales").innerHTML = `
+    <h3>Bicicletas de carretera: ${tienda.numCarretera()}</h3>
+    <h3>Bicicletas de montaña: ${tienda.numMontaña()} </h3>
+    <h3>Total de bicicletas: ${tienda.numTotal()}</h3>
+    <h3>Total de ventas: ${tienda.numVenta()}</h3>
+    `;
+}
+
+ocultarFormularios();
+let tienda = new Tienda();
+
+
+const btnAltaBici = document.getElementById("btnMostrarAlta");
+const formAlta = document.querySelector('[name="frmAltaBicicleta"]');
+btnAltaBici.addEventListener("click", ()=>{
+    ocultarFormularios();
+    formAlta.classList.remove("oculto");
+});
+
+const btnFormAlta = document.querySelector('[name="btnAltaBicicleta"]');
+btnFormAlta.addEventListener("click", altaBicicleta);
+
+
+const btnVentaBici = document.querySelector('[name="btnMostrarVenta"]');
+const formVentaBici = document.querySelector('[name="frmVentaBicicleta"]');
+btnVentaBici.addEventListener("click", ()=>{
+    ocultarFormularios();
+    formVentaBici.classList.remove("oculto");
+});
+
+const btnFormVentaBici = document.querySelector('[name="btnVentaBicicleta"]');
+
+btnFormVentaBici.addEventListener("click",ventaBici);
+
+const btnMostrarTotales = document.querySelector('[name="btnMostrarTotales"]');
+btnMostrarTotales.addEventListener("click", refrescarTotales);
--- DWEC/Ejercicio examen bicicletas/Plantilla examen/js/objetos.js
+++ DWEC/Ejercicio examen bicicletas/Plantilla examen/js/objetos.js
@@ -1,1 +1,80 @@
-﻿
+﻿"use strict";
+
+class Tienda{
+    constructor(tBicis = [],numVentas){
+        this.tBicis = tBicis;
+        this.numVentas = numVentas;
+    }
+    altaBicicleta(oBici){
+        /* Devuelve un booleano */
+
+        for(const bici of this.tBicis){
+
+            if(bici.localizador === oBici.localizador){
+                return false;
+            }
+        }
+        this.tBicis.push(oBici);
+        return true;
+    }
+    ventaBicis(sLocalizador){
+        /* Devuelve un String */
+
+        for (const bici of this.tBicis){
+            if(bici.localizador === sLocalizador){
+                if(bici.vendida){
+                    return 'Bicicleta ya vendida';
+                }else{
+                    bici.vendida = true;
+                    return 'Bicicleta vendida';
+                }
+            }
+        }
+        return 'Bicicleta no registrada';
+
+    }
+    listadoCarretera(){
+        /* Devuelve HTMLTable */
+    }
+    listadoGeneral(){
+        /* Devuelve HTMLTable */
+    }
+    numCarretera() {
+        return this.tBicis.filter(b => b instanceof Carretera).length
+      }
+      numMontaña() {
+        return this.tBicis.filter(b => b instanceof Montaña).length
+      }
+      numTotal() {
+        return this.tBicis.length
+      }
+      numVenta() {
+        return this.tBicis.filter(b => b.vendida).length
+      }
+
+}
+class Bicicleta{
+    constructor(localizador, año, vendida){
+        this.localizador = localizador;
+        this.año = año;
+        this.vendida = vendida;
+    }
+    toHTMLRow(){
+
+    }
+}
+class Carretera extends Bicicleta{
+    constructor(localizador, año, vendida, numPlatos){
+        super(localizador,año,vendida);
+        this.numPlatos = numPlatos;
+    }
+    toHTMLRow(){
+
+    }
+}
+class Montaña extends Bicicleta{
+    constructor (localizador,año,vendida,numSuspensiones){
+        super(localizador,año,vendida);
+        this.numSuspensiones = numSuspensiones;
+    }
+}
