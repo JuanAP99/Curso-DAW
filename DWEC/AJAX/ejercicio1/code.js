8b4d757ac98fac048bb5fdc2cedacb6994cffdfa
JuanAP99 <alconlebrija@gmail.com>
2023-12-19 21:29:12 +0100

    safsd

---
+++ DWEC/AJAX/ejercicio1/code.js
@@ -0,0 +1,21 @@
+function loadXMLDoc(filename, callback) {
+    let xhttp;
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    }
+    else { // código de IE5 and IE6
+      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.onreadystatechange = function (){
+      if (xhttp.readyState === XMLHttpRequest.DONE) {
+        if (xhttp.status === 200) {
+          callback(xhttp.responseXML);
+          // callback(xhttp.responseText); // si el fichero es de texto
+        } else {
+          console.log("Hubo un error con la petición.");
+        }
+      }	
+    };
+    xhttp.open("GET", filename, true); //true = asíncrona, //false = síncrona
+    xhttp.send();
+  }
